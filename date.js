b1ce832801f3497221201d553af4b74e4caf257f
4solo <34310696@qq.com>
2015-08-24 15:29:44 -0700

    add more class

--- date.js
+++ date.js
@@ -2,6 +2,7 @@ document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='classes.html'>Classes</a></li>" +
+"<li><a href='resume.html'>Resume</a></li>" +
 "<li><a href='contact.html'>Contact</a></li>" +
 "</ul>";
 document.getElementById("foot01").innerHTML =
