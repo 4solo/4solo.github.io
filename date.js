fb0b35e4a365a761265134280b8c4f0ae055ae95
4solo <34310696@qq.com>
2015-08-24 15:11:16 -0700

    add class

--- date.js
+++ date.js
@@ -1,2 +1,8 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='classes.html'>Classes</a></li>" +
+"<li><a href='contact.html'>Contact</a></li>" +
+"</ul>";
 document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " Sky Wang . All rights reserved.</p>";
